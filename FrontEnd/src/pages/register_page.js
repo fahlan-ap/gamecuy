6823663a39f2606bedcb8426f146c18898247d18
Ahmadnhy <begopeoplee@gmail.com>
2025-01-13 23:29:25 +0700

    menghapus telepon form

--- FrontEnd/src/pages/register_page.js
+++ FrontEnd/src/pages/register_page.js
@@ -56,11 +56,6 @@ const RegisterForm = () => {
                 <Form.Control type="email" placeholder="Enter email" />
               </Form.Group>
 
-              <Form.Group controlId="formBasicPhone" className="mt-3">
-                <Form.Label>Phone Number</Form.Label>
-                <Form.Control type="tel" placeholder="Enter phone number" />
-              </Form.Group>
-
               <Form.Group
                 controlId="formBasicPassword"
                 className="mt-3 position-relative"
