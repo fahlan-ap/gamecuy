c46be0d31cbf5f203303e589d6ea75368b693195
Ahmadnhy <begopeoplee@gmail.com>
2025-01-20 14:25:28 +0700

    penambahan komponen cardhome

--- FrontEnd/src/pages/home_page.js
+++ FrontEnd/src/pages/home_page.js
@@ -1,12 +1,14 @@
 import React from "react";
 import NavBar from "../components/navbar";
+import Cardhome from "../components/cardhome";
 
 const HomePage = () => {
   return (
     <>
-        <div className="bckground-home">
-          <NavBar />
-        </div>
+      <div className="bckground-home">
+        <NavBar />
+        <Cardhome />
+      </div>
     </>
   );
 };
